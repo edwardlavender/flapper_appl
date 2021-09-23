e207bb17beac8a50ba0e6779e77601458fa2f34a
Edward Lavender <el72@st-andrews.ac.uk>
2021-09-23 11:21:43 +0100

    Update examine_depth_use with key components of workflow.

--- R/define_global_param.R
+++ R/define_global_param.R
@@ -7,7 +7,7 @@
 # ... family of algorithms within this project.
 
 #### Steps preceding this code:
-# 1)
+# 1) NA
 
 
 ######################################
@@ -23,7 +23,39 @@ library(flapper)
 
 ######################################
 ######################################
-####
+#### Spatial parameters
+
+#### Define projections
+proj_wgs84 <- sp::CRS("+init=epsg:4326")
+proj_utm <- sp::CRS("+proj=utm +zone=29 +datum=WGS84 +units=m +no_defs")
+
+
+######################################
+######################################
+#### Depth time series parameters
+
+#### Depth error model
+# (tag error + tidal range + bathy error+ possible pelagic movements)
+calc_depth_error <- function(depth) {
+  e <- 4.77 + 2.5 + sqrt(0.5 ^2 + (0.013 * depth)^2)
+  e <- matrix(c(-(e + 5), e), nrow = 2)
+  return(e)
+}
+calc_depth_error(0)
+calc_depth_error(300)
+calc_depth_error <- Vectorize(calc_depth_error)
+calc_depth_error(c(0, 300))
+
+
+######################################
+######################################
+#### Acoustic parameters
+
+#### Receiver detection_range (m)
+detection_range <- 750
+
+
+
 
 
 #### End of code.
--- R/examine_depth_use.R
+++ R/examine_depth_use.R
@@ -7,24 +7,58 @@
 # ... representation in terms of depth use.
 
 #### Steps preceding this code:
-# 1)
+# 1) Processing of raw data   ... via process_data_raw.R
+# 2) Define global parameters ... via define_global_param.R
 
 
 ######################################
 ######################################
 #### Set up
 
-#### Wipe workspace
+#### Wipe workspace & load flapper & global param
 rm(list = ls())
+source("./R/define_global_param.R")
 
-#### Essential packages
-library(flapper)
+#### Load data
+archival   <- readRDS("./data/movement/tag/archival_eg.rds")
+site_bathy <- raster::raster("./data/spatial/site_bathy.tif")
+site_coast <- readRDS("./data/spatial/site_coast.rds")
 
 
 ######################################
 ######################################
-####
+#### Implement DC algorithm
 
+#### Define algorithm param
+n_cores <- 10L
+split <- floor(nrow(archival)/n_cores)
+cl <- parallel::makeCluster(n_cores)
+parallel::clusterEvalQ(cl, library(raster))
+
+#### Implement algorithm
+out_dc <- dc(archival = archival[1:50, ],
+             bathy = site_bathy,
+             calc_depth_error = calc_depth_error,
+             check_availability = TRUE,
+             # write_record_spatial_for_pf = list(filename = "./data/movement/depth_use/dc/record/", format = "GTiff"),
+             con = "./data/movement/depth_use/dc/dc_log.txt",
+             split = split,
+             cl = cl)
+
+#### Process out_dc
+out_dc_s <- acdc_simplify(out_dc, type = "dc", mask = site_bathy)
+
+#### Check availability of depth contours at each time step
+out_dc_dat <- acdc_access_dat(out_dc_s)
+table(out_dc_dat$availability)
+
+#### Process map (for plotting)
+out_dc_map <- out_dc_s$out_dc_map
+out_dc_map[is.na(out_dc_map)] <- 0
+
+#### Plot map
+prettyGraphics::pretty_map(add_rasters = list(x = out_dc_map),
+                           add_polys = list(x = site_coast, col = "dimgrey", border = "dimgrey"))
 
 #### End of code.
 ######################################
