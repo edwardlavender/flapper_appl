cffb0e69c4447d68e58376d59c7c423f79d5b8c7
Edward Lavender <el72@st-andrews.ac.uk>
2021-09-27 11:18:33 +0200

    Examine implications of DC algorithm results for sediment use.

--- R/examine_depth_use.R
+++ R/examine_depth_use.R
@@ -20,30 +20,38 @@ rm(list = ls())
 source("./R/define_global_param.R")
 
 #### Load data
-archival   <- readRDS("./data/movement/tag/archival_eg.rds")
-site_bathy <- raster::raster("./data/spatial/site_bathy.tif")
-site_coast <- readRDS("./data/spatial/site_coast.rds")
+archival       <- readRDS("./data/movement/tag/archival_eg.rds")
+site_bathy     <- raster::raster("./data/spatial/site_bathy.tif")
+site_coast     <- readRDS("./data/spatial/site_coast.rds")
+site_sediments <- readRDS("./data/spatial/site_sediments.rds")
 
 
 ######################################
 ######################################
 #### Implement DC algorithm
 
-#### Define algorithm param
-n_cores <- 10L
-split <- floor(nrow(archival)/n_cores)
-cl <- parallel::makeCluster(n_cores)
-parallel::clusterEvalQ(cl, library(raster))
-
 #### Implement algorithm
-out_dc <- dc(archival = archival[1:50, ],
-             bathy = site_bathy,
-             calc_depth_error = calc_depth_error,
-             check_availability = TRUE,
-             # write_record_spatial_for_pf = list(filename = "./data/movement/depth_use/dc/record/", format = "GTiff"),
-             con = "./data/movement/depth_use/dc/dc_log.txt",
-             split = split,
-             cl = cl)
+# This takes 1 hr 40 mins with 4 cores
+run_dc <- FALSE
+if(run_dc){
+
+  ## Define algorithm param
+  n_cores <- 4L
+  split <- floor(nrow(archival)/n_cores)
+  cl <- parallel::makeCluster(n_cores)
+  parallel::clusterEvalQ(cl, library(raster))
+
+  ## Implement algorithm
+  out_dc <- dc(archival = archival,
+               bathy = site_bathy,
+               calc_depth_error = calc_depth_error,
+               check_availability = TRUE,
+               # write_record_spatial_for_pf = list(filename = "./data/movement/depth_use/dc/record/", format = "GTiff"),
+               con = "./data/movement/depth_use/dc/dc_log.txt",
+               split = split,
+               cl = cl)
+  saveRDS(out_dc, "./data/movement/depth_use/dc/out_dc.rds")
+} else out_dc <- readRDS("./data/movement/depth_use/dc/out_dc.rds")
 
 #### Process out_dc
 out_dc_s <- acdc_simplify(out_dc, type = "dc", mask = site_bathy)
@@ -53,12 +61,66 @@ out_dc_dat <- acdc_access_dat(out_dc_s)
 table(out_dc_dat$availability)
 
 #### Process map (for plotting)
-out_dc_map <- out_dc_s$out_dc_map
+out_dc_map <- out_dc_s$map
 out_dc_map[is.na(out_dc_map)] <- 0
+out_dc_map_pc <- (out_dc_map/nrow(archival))
 
 #### Plot map
-prettyGraphics::pretty_map(add_rasters = list(x = out_dc_map),
+prettyGraphics::pretty_map(add_rasters = list(x = out_dc_map_pc),
                            add_polys = list(x = site_coast, col = "dimgrey", border = "dimgrey"))
+
+#### Map home and core ranges
+## 'home' range
+out_dc_map_home <- out_dc_map
+home <- raster::quantile(out_dc_map_home, 0.5)
+out_dc_map_home[out_dc_map_home < home]  <- 0
+out_dc_map_home[out_dc_map_home >= home] <- 1
+prettyGraphics::pretty_map(add_rasters = list(x = out_dc_map_home),
+                           add_polys = list(x = site_coast, col = "dimgrey", border = "dimgrey"))
+## 'core' range
+out_dc_map_core <- out_dc_map
+core <- raster::quantile(out_dc_map_core, 0.95)
+out_dc_map_core[out_dc_map_core < core]  <- 0
+out_dc_map_core[out_dc_map_core >= core] <- 1
+prettyGraphics::pretty_map(add_rasters = list(x = out_dc_map_core),
+                           add_polys = list(x = site_coast, col = "dimgrey", border = "dimgrey"))
+
+
+#### Relate putative patterns of depth use to sediment availability
+## 'background' versus 'used' sediment availability in region examined
+run_sediments <- FALSE
+if(run_sediments){
+  ## Background sediments
+  background_xy        <- raster::rasterToPoints(out_dc_map, fun = function(x) !is.na(x))
+  background_xy        <- sp::SpatialPoints(background_xy[, 1:2], proj4string = proj_utm)
+  background_sediments <- sp::over(background_xy, site_sediments)
+  saveRDS(background_sediments, "./data/spatial/site_sediments_background.rds")
+  ## 'Core' sediments
+  core_xy <- raster::rasterToPoints(out_dc_map_core, fun = function(x) x==1)
+  core_xy <- sp::SpatialPoints(core_xy[, 1:2], proj4string = proj_utm)
+  core_sediments <- sp::over(core_xy, site_sediments)
+  saveRDS(core_sediments, "./data/movement/depth_use/dc/core_sediments.rds")
+} else {
+  core_sediments <- readRDS("./data/movement/depth_use/dc/core_sedimenrts.rds")
+  background_sediments <- readRDS("./data/spatial/site_sediments_background.rds")
+}
+## Examine putative sediment use versus availability
+# Get sediment availability versus use proportions
+dat_sediments <- table(background_sediments$seabed_sub)/sum(table(background_sediments$seabed_sub))
+dat_sediments <- data.frame(sediment = factor(names(dat_sediments)),
+                            availability = as.numeric(dat_sediments))
+
+dat_sediments_core <- table(core_sediments$seabed_sub)/sum(table(core_sediments$seabed_sub))
+dat_sediments$core <- as.numeric(dat_sediments_core)[match(as.character(dat_sediments$sediment),
+                                                           names(dat_sediments_core))]
+dat_sediments$core[is.na(dat_sediments$core)] <- 0
+dat_sediments <- dat_sediments %>% dplyr::arrange(availability)
+# Plot differences in sediment availability versus use
+pp <- par(mfrow = c(1, 2))
+barplot(dat_sediments$availability)
+barplot(dat_sediments$core)
+par(pp)
+
 
 #### End of code.
 ######################################
